5c24d86c030be9ce9788eaa058f64eaf5ee90d7e
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-11-26 15:54:39 +0530

    added

--- router/index.js
+++ router/index.js
@@ -12,9 +12,16 @@ class Router {
       //TODO attach routes
       this._attachWebRoutes();
       //TODO handle 404 pages
+      this._handlePageNotFound();
       //TODO handle exceptions
       //TODO Register Router
     app.use(this.router)    
+  }
+
+  _handlePageNotFound(){
+    this.router.all('*',(req,res)=>{
+        res.status(404).send("page not found")
+    })
   }
 
   _attachWebRoutes(){
